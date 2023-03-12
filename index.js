af97a5a96a170c33b0eef02bc50d9871ce43db45
Rafael Finol <rafaelfinol88@gmail.com>
2023-03-11 21:29:53 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let thankYouCard = []
+    for (let i = 0; i < names.length; i++){
+        thankYouCard.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouCard;
+}
+
+function countDown(number){
+    let value = number;
+    while (value >= 0){
+        console.log(value--);
+    }
+    return number;
+}
